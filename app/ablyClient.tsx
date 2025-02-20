32e824d6c725c291622325410f9f1e5230eaf6fd
Cam Michie <cameron.oscar.michie@gmail.com>
2025-02-20 14:15:19 +0000

    add ably

---
+++ app/ablyClient.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as Ably from "ably";
+import { AblyProvider } from 'ably/react';
+import { ReactNode } from "react";
+
+interface AblyClientProps {
+  children: ReactNode;
+}
+
+const AblyClient: React.FC<AblyClientProps> = ({ children }) => {
+
+  console.log(process.env.ABLY_API_KEY);
+  const client = new Ably.Realtime({
+         authUrl: 'api/ably/authenticate',
+         logLevel: 2
+      });
+      
+  client.connection.on((stateChange) => {
+    console.log('New connection state is ' + stateChange.current);
+  });
+  return (
+    <AblyProvider client={client}>
+      {children}
+    </AblyProvider>
+  );
+};
+
+export default AblyClient;
