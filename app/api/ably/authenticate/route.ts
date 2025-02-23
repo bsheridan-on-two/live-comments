67ed5e655800ee4659e40e4e122a81d4a8890556
Cam Michie <cameron.oscar.michie@gmail.com>
2025-02-23 16:54:03 +0000

    added vercel cron job and updated readme

--- app/api/ably/authenticate/route.ts
+++ app/api/ably/authenticate/route.ts
@@ -1,25 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { SignJWT } from "jose";
 import * as Ably from "ably";
 
 const CLIENT_IDS = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"];
 const getRandomClientId = (): string => {
-  return CLIENT_IDS[Math.floor(Math.random() * CLIENT_IDS.length)];
+  let r = (Math.random() + 1).toString(36).substring(7);
+  return CLIENT_IDS[Math.floor(Math.random() * CLIENT_IDS.length)] + '_' + r;
 };
 
-const createToken = async (clientId:string, apiKey: string) => {
+export const GET = async () => {
+    const clientId = getRandomClientId();
     const client = new Ably.Rest({
       key: process.env.ABLY_API_KEY,
       clientId: clientId,
     });
     const tokenRequestData = await client.auth.createTokenRequest();
-
-  return tokenRequestData;
-}
-
-export const GET = async () => {
-    const clientId = getRandomClientId();
-    const token = await createToken(clientId, process.env.ABLY_API_KEY);
-    console.log("token request: ", token);
-    return NextResponse.json(token)
+    return NextResponse.json(tokenRequestData)
   }
